a4d32fc9b0e2fcdaad6844f636142183a6809a2d
sagun03 <sagunsaluja13@gmail.com>
2023-08-13 00:09:42 +0530

    updated

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 // import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Footer from "../components/Footer";
